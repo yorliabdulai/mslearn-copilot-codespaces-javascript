a61e0a9cd2dfbbe77e04252e27b709b903528eb9
Abdulai Yorli Iddrisu <yorliabdulai1@gmail.com>
2024-06-08 16:04:56 +0000

    chore: Update contact information in App.jsx and add CSS animation for social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abdulai Yorli Iddrisu",
+  title: "Full Stack Developer & AWS Certified Cloud Practitioner",
+  email: "yorliabdulai1@gmail.com",
+  gitHub: "https://github.com/yorliabdulai",
+  instagram: "yorliabdulai",
+  linkedIn: "https://www.linkedin.com/in/abdulai-yorli-iddrisu-4899b1241/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
